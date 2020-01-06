39b838640b378a2a903010da885790ce7751dd17
MohamedKerkeb <kerkeb.moha@gmail.com>
2020-01-06 09:30:47 +0100

    ajout de mapping

--- src/App.js
+++ src/App.js
@@ -1,75 +1,83 @@
-import React, {useState, useEffect} from 'react';
-import {MoonLoader} from 'react-spinners'
-import './App.css';
-import Header from './components/Header/Header';
-import SearchBar from './components/SearchBar/SearchBar';
-import Preview from './components/Preview/Preview'
-import WeatherDetails from './components/WeatherDetails/WeatherDetails'
+import React, { useState, useEffect } from "react";
+import { MoonLoader } from "react-spinners";
+import "./App.css";
+import Header from "./components/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Preview from "./components/Preview/Preview";
+import WeatherDetails from "./components/WeatherDetails/WeatherDetails";
 
-import Card from './elements/Card/Card'
-import ErrorNotice from './components/ErrorNotice/ErrorNotice';
-import Footer from './components/Footer/Footer';
+import Card from "./elements/Card/Card";
+import ErrorNotice from "./components/ErrorNotice/ErrorNotice";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   const [weather, setWeather] = useState([]);
-  const [loading , setLoading] = useState(false);
-  const [error, setError] = useState(false)
-  const [search, setSearch] = useState('')
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [search, setSearch] = useState("");
   // useEffect(  () => {
-    
-    const getWeather = async () => {
 
-    const city = search || 'paris';
-    const APIKEY = '284b643282f75cbed8f20c9461e64b3c';    
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=fr&appid=${APIKEY}`
-      const response = await fetch(URL).then(res => res.json()).then(data => {
-        if(data.cod === 200) {
+  const getWeather = async () => {
+    const city = search || "paris";
+    const APIKEY = "284b643282f75cbed8f20c9461e64b3c";
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=fr&appid=${APIKEY}`;
+    const response = await fetch(URL)
+      .then(res => res.json())
+      .then(data => {
+        if (data.cod === 200) {
           setWeather({
             city: data.name,
             temperature: data.main.temp,
             description: data.weather[0].main
           });
-          setLoading(false)
+          setLoading(false);
         } else {
-          throw data.cod
+          throw data.cod;
         }
-      }).catch(err => {console.log(err); setLoading(false); setError(true) })
-    };
+      })
+      .catch(err => {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      });
+  };
   //   getWeather()
   //  }, [])
 
-  const searchBar = (evt) => {
-    evt.preventDefault()
-    setSearch(evt.target.value)
-  }
+  const searchBar = evt => {
+    evt.preventDefault();
+    setSearch(evt.target.value);
+  };
 
-  const tryAgain = () =>{
-    console.log('tryAgain')
-    setSearch('');
+  const tryAgain = () => {
+    console.log("tryAgain");
+    setSearch("");
     setWeather([]);
-    setError(false)
+    setError(false);
+  };
+
+  let cardContent = <Preview />;
+  if (loading) {
+    cardContent = <MoonLoader />;
+  } else if (weather.temperature && weather.description !== "") {
+    cardContent = <WeatherDetails data={weather} />;
+  } else if (error) {
+    cardContent = <ErrorNotice onClickHandler={() => tryAgain()} />;
   }
 
-  let cardContent = <Preview />
-  if(loading) {
-    cardContent = <MoonLoader />
-  } else if( weather.temperature && weather.description !=='')  {
-    cardContent = < WeatherDetails data={weather}/>
-  } else if( error ) {
-    cardContent = <ErrorNotice onClickHandler={() => tryAgain()}/>
-  }
-  
   return (
     <div className="AppWrapper">
       {console.log(weather)}
-      <Header  onClickHandler={() => tryAgain()}/>
-      <main className='AppMain'>
-        <SearchBar value={search} onChangeHandler={searchBar} onClickHandler={() => getWeather()}/>
-        <Card>
-          {cardContent}
-        </Card>
+      <Header onClickHandler={() => tryAgain()} />
+      <main className="AppMain">
+        <SearchBar
+          value={search}
+          onChangeHandler={searchBar}
+          onClickHandler={() => getWeather()}
+        />
+        <Card>{cardContent}</Card>
       </main>
-      <Footer onClickHandler={() => tryAgain()}/>
+      <Footer onClickHandler={() => tryAgain()} />
     </div>
   );
 }
